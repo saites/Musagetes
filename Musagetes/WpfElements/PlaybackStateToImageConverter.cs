0d53e8d02849cca986bd0c58e937ad1c5a41dd11
saites <saites2001@gmail.com>
2015-04-07 17:38:37 -0700

    Added resources

---
+++ Musagetes/WpfElements/PlaybackStateToImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Musagetes.WpfElements
+{
+    class PlaybackStateToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is PlaybackControlBehavior.Playback)) return value;
+            switch ((PlaybackControlBehavior.Playback)value)
+            {
+                case PlaybackControlBehavior.Playback.Play:
+                    return Properties.Resources.PlayButton;
+                case PlaybackControlBehavior.Playback.Pause:
+                    return Properties.Resources.PauseButton;
+                case PlaybackControlBehavior.Playback.Stop:
+                    return Properties.Resources.StopButton;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
