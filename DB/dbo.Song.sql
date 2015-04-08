d7266c47ab1d5fc50fc134482189bc6c59ecfb1d
saites <saites2001@gmail.com>
2015-04-07 18:10:42 -0700

    cleaned up repo

--- DB/dbo.Song.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Song]
-(
-	[SongId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [SongTitle] VARCHAR(MAX) NOT NULL, 
-    [PrimaryArtist] VARCHAR(MAX) NULL, 
-    [Seconds] INT NOT NULL
-)
